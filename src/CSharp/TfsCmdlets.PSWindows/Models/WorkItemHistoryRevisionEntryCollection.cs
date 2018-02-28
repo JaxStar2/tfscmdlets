cb694d2203cfa2b510ddcf59c4f2fd294fc32f5d
Igor Abade (MSA) <igoravl@hotmail.com>
2018-02-28 01:10:42 -0300

    Move service providers from Cmdlet project to TfsApi project

--- src/CSharp/TfsCmdlets.PSWindows/Models/WorkItemHistoryRevisionEntryCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TfsCmdlets.Models
-{
-    public class WorkItemHistoryRevisionEntryCollection : List<WorkItemHistoryRevisionEntry>
-    {
-        public WorkItemHistoryRevisionEntry this[string referenceName] => Find(f => f.ReferenceName.Equals(referenceName));
-    }
-}
