d82a248a63812bf9b2b4b1e7e5482689eb2f9ef3
Igor Abade (MSA) <igoravl@hotmail.com>
2018-03-20 14:10:23 -0300

    Add documentation comments

--- src/CSharp/TfsCmdlets.PSWindows/Cmdlets/Connection/ConnectConfigurationServer.cs
+++ src/CSharp/TfsCmdlets.PSWindows/Cmdlets/Connection/ConnectConfigurationServer.cs
@@ -10,6 +10,26 @@ namespace TfsCmdlets.Cmdlets.Connection
     ///     Functions that operate on a server level (as opposed to those operation on a team project collection level) 
     ///     will use by default a connection opened by this function.</para>
     /// </summary>
+    /// <list type="alertSet">
+    ///   <item>
+    ///     <description>A TFS Configuration Server represents the server that is running Team Foundation Server. 
+    ///         On a database level, it is represented by the Tfs_Configuration database. Operations that should be 
+    ///         performed on a server level (such as setting server-level permissions) require a connection to a TFS 
+    ///         configuration server. Internally, this connection is represented by an instance of the 
+    ///         Microsoft.TeamFoundation.Client.TfsConfigurationServer class and is kept in a PowerShell global variable 
+    ///         caled TfsServerConnection.</description>
+    ///   </item>
+    /// </list>
+    /// <example>
+    ///   <code>PS&gt; Connect-TfsConfigurationServer -Server http://vsalm:8080/tfs </code>
+    ///   <para>Connects to the TFS server specified by the URL in the Server argument</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS&gt; Connect-TfsConfigurationServer -Server vsalm</code>
+    ///   <para>Connects to a previously registered TFS server by its user-defined name "vsalm". For more information, see Get-TfsRegisteredConfigurationServer</para>
+    /// </example>
+    /// <para type="link">Microsoft.TeamFoundation.Client.TfsConfigurationServer</para>
+    /// <para type="link">https://blogs.msdn.microsoft.com/taylaf/2010/02/23/introducing-the-tfsconnection-tfsconfigurationserver-and-tfsteamprojectcollection-classes/</para>
     [Cmdlet(VerbsCommunications.Connect, "ConfigurationServer", DefaultParameterSetName = "Explicit credentials")]
     [OutputType("Microsoft.TeamFoundation.Client.TfsConfigurationServer,Microsoft.TeamFoundation.Client")]
     public class ConnectConfigurationServer : ServerLevelCmdlet
@@ -32,6 +52,8 @@ namespace TfsCmdlets.Cmdlets.Connection
 
         /// <summary>
         /// <para type="description">${HelpParam_Server}</para>
+        /// <para type="inputType">Microsoft.TeamFoundation.Client.TfsConfigurationServer, System.String, System.Uri</para>
+        /// <para type="inputType">You can pipe a TfsConfigurationServer object, a registered server name or a URL pointing to a TFS instance (either as a String or Uri object)</para>
         /// </summary>
         [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
         [ValidateNotNull]
